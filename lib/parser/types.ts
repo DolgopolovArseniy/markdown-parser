d7a6cacb0c8f4ab834b48ae2373c4524417b6d14
Arseniy Dolgopolov <hellrenf@gmail.com>
2026-07-13 11:22:18 +0300

    feat(block-parser): define core interface and type definitions for block parser

--- lib/parser/types.ts
+++ lib/parser/types.ts
@@ -7,18 +7,56 @@
 //
 // TODO: определить типы Token и AST-узлов вручную
 
+export type HeadingNode = {
+  type: 'Heading';
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+  text: string;
+};
+
+export type ParagraphNode = {
+  type: 'Paragraph';
+  text: string;
+};
+
+export type CodeBlockNode = {
+  type: 'CodeBlock';
+  text: string;
+};
+
+export type UnorderedListItemNode = {
+  type: 'UnorderedListItem';
+  text: string;
+};
+
+export type OrderedListItemNode = {
+  type: 'OrderedListItem';
+  listIndex: number;
+  text: string;
+};
+
+export type DocumentNode = {
+  type: 'Document';
+  children: AstNode[];
+};
+
 export type Token =
-  | { type: 'Heading'; level: 1 | 2 | 3 | 4 | 5 | 6; text: string }
-  | { type: 'Paragraph'; text: string }
-  | {
-      type: 'UnorderedListItem';
-      text: string;
-    }
-  | {
-      type: 'OrderedListItem';
-      listIndex: number;
-      text: string;
-    }
-  | { type: 'CodeBlock'; text: string }
+  | HeadingNode
+  | ParagraphNode
+  | UnorderedListItemNode
+  | OrderedListItemNode
+  | CodeBlockNode
   | { type: 'BlankLine' };
-export type AstNode = never; // TODO: заменить на discriminated union (узел AST)
+
+export type AstNode =
+  | DocumentNode
+  | HeadingNode
+  | ParagraphNode
+  | {
+      type: 'UnorderedList';
+      children: UnorderedListItemNode[];
+    }
+  | {
+      type: 'OrderedList';
+      children: OrderedListItemNode[];
+    }
+  | CodeBlockNode;
